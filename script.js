5802f0bdcf52a5a3d60d72a2893105bf1e815b79
alimnh520 <alimnh412@gmail.com>
2024-07-11 17:37:57 +0600

    change value of script.js

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 const circle = document.querySelector(".container");
 
-circle.addEventListener("click", () => {
-    for (let i = 0; i < 50; i++) {
-        
-        let ball = document.createElement("span");
-        ball.classList.add("ball");
-        document.body.appendChild(ball);
-
-        let randomX = (Math.random() - 0.5) * window.innerWidth;
-        let randomY = (Math.random() - 0.5) * window.innerHeight;
-        ball.style.setProperty("--x", randomX+"px");
-        ball.style.setProperty("--y", randomY+"px");
-
-        let randomSize = Math.random() * 60 + 10;
-
-        ball.style.width = randomSize + "px";
-        ball.style.height = randomSize + "px";
-
-        let duration = Math.random() * 3 + 2;
-
-        ball.style.animation = ` ballMove ${duration}s ease`;
-
-        setTimeout(() => {
-            ball.remove();
-        },5000);
-    }
-    console.log(Math.random() * 60 + 10);
-});
+function bubble() {
+  for (let i = 0; i < 50; i++) {
+  
+    let ball = document.createElement("span");
+    ball.classList.add("ball");
+    document.body.appendChild(ball);
+  
+    let randomX = (Math.random() - 0.5) * window.innerWidth;
+    let randomY = (Math.random() - 0.5) * window.innerHeight;
+    ball.style.setProperty("--x", randomX + "px");
+    ball.style.setProperty("--y", randomY + "px");
+  
+    let randomSize = Math.random() * 60 + 10;
+  
+    ball.style.width = randomSize + "px";
+    ball.style.height = randomSize + "px";
+  
+    let duration = Math.random() * 3 + 2;
+  
+    ball.style.animation = ` ballMove ${duration}s ease`;
+  
+    setTimeout(() => {
+      ball.remove();
+    }, 5000);
+  }
+}
+bubble();
+setInterval(() => {
+  bubble();
+},5000);
